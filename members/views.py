6f921ad0d9be8901b6581f88111dfcf1e0016b50
nattyait <nattanicha@gmail.com>
2020-10-07 11:36:15 +0700

    Manage level

--- members/views.py
+++ members/views.py
@@ -24,13 +24,34 @@ class MemberView(TemplateView):
 
     def get(self, request, *args, **kwargs):
         member_id = self.kwargs['member_id']
-        print(member_id)
         member = Member.objects.get(member_id=member_id)
+        member_card = ''
+        level = member.level
+        if level == 'Grand Dealer':
+            member_card = '/card_templates/grand_dealer.jpg'
+        elif level == 'Dealer':
+            member_card = '/card_templates/dealer.jpg'
+        elif level == 'Super VIP Gold':
+            member_card = '/card_templates/sup_VIP_gold.jpg'
+        elif level == 'VIP Gold':
+            member_card = '/card_templates/VIP_gold.jpg'
+        elif level == 'VIP':
+            member_card = '/card_templates/VIP.jpg'
+        elif level == 'ตัวแทนหลัก':
+            member_card = '/card_templates/level2.jpg'
+        elif level == 'ตัวแทนย่อย':
+            member_card = '/card_templates/level1.jpg'
+        elif level == 'Start':
+            member_card = '/card_templates/start.jpg'
         return render(
             request,
             self.template,
             {
             'member': member,
-            'member_card': '/card_templates/dealer.jpg',
+            'member_card': member_card,
             }
         )
+        
+    '''
+    Todo if level, choose template, then open image and put on the level template, and render
+    '''
