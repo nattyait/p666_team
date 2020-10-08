f995cddf44aebad797ff9beae6ea4caa11b1b6c1
nattyait <nattanicha@gmail.com>
2020-10-08 21:41:44 +0700

    Use postgres

--- p666_team/settings.py
+++ p666_team/settings.py
@@ -81,8 +81,12 @@ WSGI_APPLICATION = 'p666_team.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'p666', 
+        'USER': 'postgres', 
+        'PASSWORD': '1234',
+        'HOST': '127.0.0.1', 
+        'PORT': '5432',
     }
 }
 
